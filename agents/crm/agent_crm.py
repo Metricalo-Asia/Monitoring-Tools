88dddd8d3ad4f800e2fd0b4b4cf03fea660988fd
Arifuzzaman Pranto <arifuzzamanpranto@ymail.com>
2024-09-30 19:06:12 +0600

    Updated agent_crm_plans.py to agent_crm.py

--- agents/crm/agent_crm.py
+++ agents/crm/agent_crm.py
@@ -5,7 +5,7 @@ import requests
 from bs4 import BeautifulSoup
 
 
-class AgentCRMPricePlan:
+class AgentCRM:
     url = None
     site_api_key = None
     site_api_this_webshop_url = None
@@ -13,13 +13,13 @@ class AgentCRMPricePlan:
     merchant_name = None
     response = None
     has_error = False
+
     def __init__(self, row):
         self.row = row
         self.url = row['url']
-        self.site_api_this_webshop_url = f"{os.getenv("CRM_HOST")}/api/s/v3/this/webshop"
+        self.site_api_this_webshop_url = f"{os.getenv('CRM_HOST')}/api/s/v3/this/webshop"
         self.site_api_key = row['site_api_key']
         self.merchant_name = row['company_name']
-
 
     def process(self):
         print("Running: " + self.__class__.__name__)
@@ -34,39 +34,43 @@ class AgentCRMPricePlan:
                 "crm_plans": len(results),
             }
 
-
         headers = {
             'x-api-key': self.site_api_key
         }
-        response = requests.get(self.site_api_this_webshop_url,None,headers=headers);
+        response = requests.get(self.site_api_this_webshop_url, None, headers=headers);
 
         if response.status_code != 200:
             self.has_error = (len(results) == 0)
             return {
                 "URL": self.url,
                 "crm_plans_status": "INVALID SITE API KEY OR CRM HOST",
-                "crm_plans": len(results),
+                "crm_plans": results,
             }
 
         response = json.loads(response.content)
 
         if "data" in response.keys():
-            if "products" not in response["data"].keys():
+            if "products" in response["data"].keys():
+                return {
+                    "URL": self.url,
+                    "crm_plans_status": "OK",
+                    "crm_data": response['data'],
+                }
+            else:
                 self.has_error = (len(results) == 0)
                 return {
                     "URL": self.url,
                     "crm_plans_status": "INVALID PRODUCT RESPONSE FROM CRM",
-                    "crm_plans": len(results),
+                    "crm_data": results,
                 }
         else:
             self.has_error = (len(results) == 0)
             return {
                 "URL": self.url,
                 "crm_plans_status": "INVALID DATA RESPONSE FROM CRM",
-                "crm_plans": len(results),
+                "crm_data": results,
             }
 
-        print(response["data"]["products"])
         #
         # soup = BeautifulSoup(self.response.content, 'html.parser')
         #
